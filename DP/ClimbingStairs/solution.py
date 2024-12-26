e45d1be009323577d8c08cf56045891ea53da9e4
Sarah Chopra <chopra.sar@gmail.com>
2024-12-25 18:08:37 -0800

    Update solution.py

--- DP/ClimbingStairs/solution.py
+++ DP/ClimbingStairs/solution.py
@@ -1,5 +1,7 @@
 
-"""you are climbing a staircase. It takes n steps to reach the top.
+"""
+This is reverse fiboncci
+you are climbing a staircase. It takes n steps to reach the top.
 
 #Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 explain
