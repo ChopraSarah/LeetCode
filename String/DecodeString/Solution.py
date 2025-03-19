c718d16465f487191206f8fd0e007df3862f7af6
Sarah Chopra <chopra.sar@gmail.com>
2025-03-19 15:26:20 -0700

    Update Solution.py

--- String/DecodeString/Solution.py
+++ String/DecodeString/Solution.py
@@ -1,4 +1,4 @@
-/*
+"""
 Given an encoded string, return its decoded string.
 
 The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
@@ -29,11 +29,7 @@ Constraints:
 s consists of lowercase English letters, digits, and square brackets '[]'.
 s is guaranteed to be a valid input.
 All the integers in s are in the range [1, 300].
-*/
-
-
-
-
+"""
 class Solution:
     
     def decodeString(self, s: str) -> str:
